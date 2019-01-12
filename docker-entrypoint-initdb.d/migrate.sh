7097787a143d6aa0fe201ecd1ce14694f6b7e92f
Maxwell Rediker <maxwell.rediker@gmail.com>
2019-01-12 13:17:20 -0800

    successfully populated database but you have to do it manually - the migrate.sh file doesnt run in the PSQL container - moving to another branch in order to clear out comments

--- docker-entrypoint-initdb.d/migrate.sh
+++ docker-entrypoint-initdb.d/migrate.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-apk add python3-dev
-# dont need makemigrations
-# python manage.py makemigrations && 
+# apk add python3-dev
+# install env variables (not the right relative path)
+# if the database exists delete it first and run these commands
+
+source democracylab_environment_variables.sh
+
 python manage.py migrate
 
-# it wouldnt to access manage.py because its not in this service unless we volumize it and share it accross services
+# it wouldnt to access manage.py because its not in this service unless we volumize it and share it across services
 
-#running priv commands in pg container
+#running privledged commands in pg container
